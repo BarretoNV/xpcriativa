01eb06dd60ce7617bd8b2880964cf2c05022dc3e
Guilherme Barreto <48062197+BarretoNV@users.noreply.github.com>
2022-02-12 00:06:04 -0300

    sidebar colapsable

--- src/components/barralateral/index.js
+++ src/components/barralateral/index.js
@@ -1,48 +1,56 @@
-import React from 'react';
-import './styles.scss';
-import IMAGES from '../../images/images.js';
+import React, { useState } from "react";
+import "./styles.scss";
+import IMAGES from "../../images/images.js";
+import { GrMenu } from "react-icons/gr";
+import { GrFormClose } from "react-icons/gr";
 
 function Sidebar() {
-    return (
-        <div className="sidebarBody">
-            <section>
-                <img src={IMAGES.XPLogo} alt="XPLogo"/>
-            </section>
-            <body>
-                <a href="/feed">
-                    <img src={IMAGES.HouseIcon} alt="House icon"/>
-                    Feed
-                </a>
-                <a href="/descobrir">
-                    <img src={IMAGES.HashIcon} alt="Hash Icon"/>
-                    Descobrir criativo
-                </a>
-                <a href="/chat">
-                    <img src={IMAGES.ChatIcon} alt="Chat Icon"/>
-                    Troca de experiências
-                </a>
-                <a href="/awards">
-                    <img src={IMAGES.TrophyIcon} alt="Trophy Icon"/>
-                    Recompensas
-                </a>
-                <a href="/profile">
-                    <img src={IMAGES.ProfileIcon} alt="Profile Icon"/>
-                    Perfil
-                </a>
-            </body>
-            <footer>
-                <div class="profileResume">
-                    <img src={IMAGES.ProfilePic} alt="Profile Icon"/>
-                    <div class="nameAndWork">
-                        
-                        <h2>@NOME</h2>
-                        <h3>Professora</h3>
+  const [sidebar, setSideBar] = useState(false);
 
-                    </div>
-                </div>
-            </footer>
+  const showSidebar = () => setSideBar(!sidebar);
+
+  return (
+    <div className={sidebar ? "sidebarBody active" : "sidebarBody"}>
+      <section>
+        <img src={IMAGES.XPLogo} alt="XPLogo" />
+        <button onClick={showSidebar}>
+          <div className="closeButton"><GrFormClose /></div> 
+          <div className="menuButton"><GrMenu /></div>
+        </button>
+      </section>
+      <div className="body">
+        <a href="/feed">
+          <img src={IMAGES.HouseIcon} alt="House icon" />
+          <p>Feed</p>
+        </a>
+        <a href="/descobrir">
+          <img src={IMAGES.HashIcon} alt="Hash Icon" />
+          <p>Descobrir criativo</p>
+        </a>
+        <a href="/chat">
+          <img src={IMAGES.ChatIcon} alt="Chat Icon" />
+          <p>Troca de experiências</p>
+        </a>
+        <a href="/awards">
+          <img src={IMAGES.TrophyIcon} alt="Trophy Icon" />
+          <p>Recompensas</p>
+        </a>
+        <a href="/profile">
+          <img src={IMAGES.ProfileIcon} alt="Profile Icon" />
+          <p>Perfil</p>
+        </a>
+      </div>
+      <footer>
+        <div class="profileResume">
+          <img src={IMAGES.ProfilePic} alt="Profile Icon" />
+          <div class="nameAndWork">
+            <h2>@NOME</h2>
+            <h3>Professora</h3>
+          </div>
         </div>
-    )
+      </footer>
+    </div>
+  );
 }
 
 export default Sidebar;
