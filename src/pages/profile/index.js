61261f041c21d5662ae2f05fd52b8a2326e70ad4
Guilherme Barreto <48062197+BarretoNV@users.noreply.github.com>
2022-02-18 16:43:16 -0300

    página de perfil iniciada

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,18 +1,85 @@
-import React from 'react';
-import './styles.scss';
-import Sidebar from '../../components/barralateral';
+import React from "react";
+import "./styles.scss";
+import Sidebar from "../../components/barralateral";
+import IMAGES from "../../images/images.js";
 
 function profile() {
-    return (
-        <div className="profileBody">
-            <Sidebar />
-            <section className="profileSection">
-
-                <h1>Perfil</h1>
-
-            </section>
+  return (
+    <div className="profileBody">
+      <Sidebar />
+      <section className="profileSection">
+        <div className="profileMain">
+          <main>
+            <div className="profileInfos">
+              <div class="profileResume">
+                <img src={IMAGES.ProfilePic} alt="Profile Icon" />
+                <div class="nameAndWork">
+                  <h2>@NOME</h2>
+                  <h3>Professora</h3>
+                </div>
+              </div>
+              <div className="profileDescription">
+                <p>
+                  Professora na Escola Guadiões da Galáxia. Apaixonada por
+                  livros e ideias criativas. #DIY #thinkoutsidethebox
+                </p>
+              </div>
+            </div>
+            <div className="profileFollows">
+              <p>XX seguidores</p>
+              <p>XX seguindo</p>
+            </div>
+          </main>
         </div>
-    )
+        <div className="profilePosts">
+          <div className="postBody">
+            <div className="postOwner">
+              <img src={IMAGES.ProfilePic} alt="Profile Pic" />
+              <div className="ownerInfo">
+                <h1>@NOME</h1>
+                <h2>Professora</h2>
+              </div>
+            </div>
+            <div className="postContent">
+              <p>lorem ipsum dolor sit amet, consectet</p>
+              <img src={IMAGES.ExamplePic} alt="example pic" />
+            </div>
+            <div className="postInteractions">
+              <div className="postReactions">
+                <button type="button">
+                  <img src={IMAGES.HeartIcon} alt="heart icon" />
+                </button>
+                <p>20</p>
+                <button type="button">
+                  <img src={IMAGES.CommentIcon} alt="comment icon" />
+                </button>
+                <p>20</p>
+                <button type="button">
+                  <img src={IMAGES.SaveIcon} alt="save icon" />
+                </button>
+                <p>20</p>
+              </div>
+              <div className="postType">
+                <img src={IMAGES.HashIcon} alt="hash icon" />
+                <h3>Já testei!</h3>
+              </div>
+            </div>
+            <div className="postComments">
+              <p>
+                <b>@NOME:</b> Amet duis sit cillum est. Consectetur amet
+                excepteur enim dolor quis et do nisi enim. Eiusmod culpa amet eu
+                laboris deserunt. Sit exercitation sunt id est ex ut laboris.
+              </p>
+            </div>
+            <div className="leaveComment">
+              <img src={IMAGES.ProfilePic} alt="ProfilePic" />
+              <input type="text" placeholder="Deixe um comentário" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default profile;
